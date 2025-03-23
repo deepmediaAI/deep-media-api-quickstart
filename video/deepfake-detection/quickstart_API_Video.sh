f1ab06ca035cf0555f7144e5075ce485a67152d1
OfmanAI <105573972+OfmanAI@users.noreply.github.com>
2025-03-23 11:56:13 -0600

    Update quickstart_API_Video.sh

--- video/deepfake-detection/quickstart_API_Video.sh
+++ video/deepfake-detection/quickstart_API_Video.sh
@@ -63,7 +63,7 @@ PROCESS_RESPONSE=$(curl -s -X POST "https://api.deepidentify.ai/v2/file/process"
      -d "{
            \"modalities\": [\"video\"],
            \"mode\": \"async\",
-           \"s3Location\": \"${UPLOADED_FILENAME}\"
+           \"s3Locations\": \"${UPLOADED_FILENAME}\"
          }")
 echo "Process Response:"
 echo "${PROCESS_RESPONSE}"
